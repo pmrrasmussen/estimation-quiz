1e7b7fe71c876639455b0d2fdabe1181267ac232
Peter Rasmussen <pmrrasmussen@icloud.com>
2023-05-28 18:09:09 +0200

    Added necessary pages, html, and css

--- estimation_quiz/quiz/urls.py
+++ estimation_quiz/quiz/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 app_name = "quiz"
 urlpatterns = [
+    path("", views.home, name='home'),
+    path("standings/", views.standings, name='standings'),
+    path("questions/", views.questions, name='questions'),
+    path("rules/", views.rules, name='rules'),
 ]
--- estimation_quiz/quiz/views.py
+++ estimation_quiz/quiz/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+from django.contrib.auth.models import User
+
+from .models import Question, Result
+
+
+def standings(request):
+    contestants = User.objects.all()
+    questions = Question.objects.order_by('id').all()
+
+    return render(
+        request,
+        'standings.html',
+        {
+            'contestants': contestants,
+            'questions': questions,
+        },
+    )
+
+
+def login(request):
+    pass
+
+
+def home(request):
+    return HttpResponseRedirect(reverse('quiz:rules'))
+
+
+def questions(request):
+    questions = Question.objects.order_by('id').all()
+
+    return render(
+        request,
+        'questions.html',
+        {
+            'questions': questions,
+        },
+    )
+
+
+def rules(request):
+    return render(request, 'rules.html')
+
+
+def answer(request, question_id, answer_high: int, answer_low: int):
+    pass
+
